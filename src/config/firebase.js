b960ad7c1263e38ca07e3bdef97422b05f65d881
iamRed-One <oladelebidemiweb@gmail.com>
2026-03-29 18:10:44 +0100

    fix: crash issues

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
@@ -18,5 +17,5 @@ export const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
 export const db = getFirestore(app);
-export const storage = getStorage(app);
+
 export default app;
